d472e00a1fa8cd4aa8987d3eadc44d482d50a995
kricn <mnyt2436@126.com>
2020-12-03 23:12:14 +0800

    admin login调整

--- admin/src/views/login/LoginForm.js
+++ admin/src/views/login/LoginForm.js
@@ -2,6 +2,7 @@ import { Component, Fragment } from 'react';
 
 //ANTD
 import { Button, Form, Input } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 //SCSS
 import './index.scss'
@@ -12,29 +13,34 @@ class LoginForm extends Component {
     this.state = {}
   }
 
+  onFinish = values => {
+    console.log(values)
+  }
+
   render() {
     return  (
         <Fragment>
             <Form
               className="login"
+              onFinish={this.onFinish}
             >
 
               <Form.Item
                 name="username"
                 rules={[ { required: true, message: '请输入用户名！' } ]}
               >
-                <Input placeholder="用户名" />
+                <Input size="large" prefix={<UserOutlined />} placeholder="用户名"  />
               </Form.Item>
 
               <Form.Item
                 name="password"
                 rules={[ { required: true, message: '请输入密码！' } ]}
               >
-                <Input placeholder="密码" />
+                <Input.Password size="large" prefix={<LockOutlined />} placeholder="密码" />
               </Form.Item>
 
               <Form.Item>
-                <Button type="primary" block>登录</Button>
+                <Button type="primary" htmlType="submit" block>登录</Button>
               </Form.Item>
 
             </Form>
--- admin/src/views/login/RegisterForm.js
+++ admin/src/views/login/RegisterForm.js
@@ -1,6 +1,7 @@
 import { Component, Fragment } from 'react';
 
 import { Button, Form, Input } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 class RegisterForm extends Component {
   constructor() {
@@ -8,29 +9,34 @@ class RegisterForm extends Component {
     this.state = {}
   }
 
+  onFinish = values => {
+    console.log(values)
+  }
+
   render() {
     return  (
       <Fragment>
       <Form
         className="register"
+        onFinish={this.onFinish}
       >
 
         <Form.Item
           name="username"
           rules={[ { required: true, message: '请输入用户名！' } ]}
         >
-          <Input placeholder="用户名" />
+          <Input size="large" prefix={<UserOutlined />} placeholder="用户名" />
         </Form.Item>
 
         <Form.Item
           name="password"
           rules={[ { required: true, message: '请输入密码！' } ]}
         >
-          <Input placeholder="密码" />
+          <Input.Password size="large" prefix={<LockOutlined />} placeholder="密码" />
         </Form.Item>
 
         <Form.Item>
-          <Button type="primary" block>注册</Button>
+          <Button type="primary" htmlType="submit" block>注册</Button>
         </Form.Item>
 
       </Form>
