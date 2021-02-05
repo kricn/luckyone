83685d0ca134b9522946982d9bb6df002424a8e2
陈凯旋 <dev_ckx@mtm.ink>
2021-02-05 19:01:56 +0800

    web cover布局

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -15,11 +15,13 @@ module.exports = {
   },
 
   loading: {
-    color: 'red'
+    color: ''
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    './assets/scss/main.scss'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -44,7 +46,7 @@ module.exports = {
   ],
   styleResources: {
     //全局css
-    // scss: ''
+    scss: []
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container">
-    <div class="title">{{list}}</div>
+    <div class="cover">
+      <div id="scene" :style="{height:boxH}">
+        <div class="layer" data-depth="0.4" :style="layerStyle">
+          <img id="image" :style="imgStyle" src="../assets/cover.png" width="1920" height="1080">
+        </div>
+      </div>
+    </div>
     <NuxtLink to="/about">About Page</NuxtLink>
   </div>
 </template>
 
 <script>
+import Parallax from 'parallax-js'
 export default {
   async asyncData(context) {
     const data = await context.$axios.get('/article')
@@ -16,17 +23,64 @@ export default {
   data() {
     return {
       list: [],
+      layerStyle: {},
+      imgStyle: {},
+      boxW: '100%', // 视口宽度
+      boxH: '100%', // 视口高度
+    }
+  },
+  methods: {
+    init(){
+      //初始化高度
+			this.boxH = document.documentElement.clientHeight + 'px';
+			this.boxW = document.documentElement.clientWidth + 'px';
+			this.coverLayer()
+    },
+    coverLayer() {
+      let scene = document.getElementById('scene'),  // 获取承载图片容器
+          _w = parseInt(this.boxW),  // 保留一份视口宽高
+          _h = parseInt(this.boxH),
+          x,  // 图片横向和纵向放大的大小
+          y,
+          i,  // 临时变量
+          e = ( _w >= 1000 || _h >= 1000) ? 1000 : 500;  //图片宽高比率，计算图片放大
+      if ( _w > _h) {  //视口宽>视口高
+        i = _w / e * 50
+        y = i
+        i = i * _w / _h
+      } else {
+        i = _h / e * 50;
+        x = i;
+        y = i * _h / _w;
+      }
+      const style = {
+        width: _w + x + 'px',
+        height: _h + y + 'px',
+        marginLeft: - 0.5 * x + 'px',
+        marginTop: - 0.5 * y + 'px'
+      }
+      this.layerStyle = Object.assign({}, this.layerStyle, style)
+      this.coverImg()
+    },
+    coverImg() {
+      
     }
   },
   mounted() {
+    // const scene = document.getElementById('scene');
+		// const parallaxInstance = new Parallax(scene, {
+		// 	relativeInput: true,
+		// 	clipRelativeInput: true,
+		// })
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .container {
-  .title {
-    color: red;
+  .cover{
+    width: 100%;
+    height: 100vh;
   }
 }
 </style>
